b9f49062ea176f49e5196941506e89e866365e54
kovalev <kov92av@gmail.com>
2022-03-01 23:14:06 +0300

    add cart page

---
+++ HomeStore/Models/Cart.cs
@@ -0,0 +1,37 @@
+namespace HomeStore.Models
+{
+    public class Cart
+    {
+        private List<CartItem> itemCollection = new List<CartItem>();
+        public virtual void AddItem(Product product, int quantity)
+        {
+            CartItem? item = itemCollection.Where(p => p.Product.Id == product.Id).FirstOrDefault();
+            if (item == null)
+            {
+                itemCollection.Add(new CartItem { Product = product, Quantity = quantity });
+            }
+            else
+            {
+                item.Quantity += quantity;
+            }
+        }
+
+        public virtual void RemoveItem(Product product)
+        {
+            itemCollection.RemoveAll(i => i.Product.Id == product.Id);
+        }
+
+        public virtual decimal ComputeTotalValue()
+        {
+            return itemCollection.Sum(e => e.Product.Price * e.Quantity);
+        }
+
+        public virtual void Clear()
+        {
+            itemCollection.Clear();
+        }
+
+        public virtual IEnumerable<CartItem> Items => itemCollection;
+
+    }
+}
