a288d24bedc5aa5b2dd47236f6c472806714fde6
kovalev <kov92av@gmail.com>
2022-03-04 23:07:02 +0300

    add migrations shipped

---
+++ HomeStore/Migrations/20220304200603_Shipped.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeProjectStudy.Migrations
+{
+    public partial class Shipped : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Shipped",
+                table: "Orders",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Shipped",
+                table: "Orders");
+        }
+    }
+}
