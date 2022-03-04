fe11f4dd85235728f32e3944499f9c6badf3b889
kovalev <kov92av@gmail.com>
2022-03-04 16:34:02 +0300

    update order controller and view checkout

--- HomeStore/Controllers/OrderController.cs
+++ HomeStore/Controllers/OrderController.cs
@@ -1,14 +1,41 @@
 using HomeStore.Models;
 using Microsoft.AspNetCore.Mvc;
+using HomeStore.Data;
 
 namespace HomeStore.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IOrderRepository orderRepository;
+        private readonly Cart cart;
+
+        public OrderController(IOrderRepository repository, Cart cart)
+        {
+            this.cart = cart;
+            orderRepository = repository;
+        }
         public ViewResult Checkout()
         {
             return View(new Order());
         }
 
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (cart.Items.Count() == 0)
+            {
+                ModelState.AddModelError("", "Корзина пуста!");
+            }
+            if (ModelState.IsValid)
+            {
+                order.Items.ToArray();
+                orderRepository.SaveOrder(order);
+                cart.Clear();
+                return RedirectToPage("/Completed", new { orderId = order.Id });
+            }
+            else
+                return View();
+
+        }
     }
 }
