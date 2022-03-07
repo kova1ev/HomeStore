7f43b12737acb18a41b5d333c6838888d3a3024d
kovalev <kov92av@gmail.com>
2022-03-07 15:51:52 +0300

    add lohin model

---
+++ HomeStore/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeStore.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Password { get; set; }
+        public string ReturnUrl { get; set; } = "/";
+    }
+}
