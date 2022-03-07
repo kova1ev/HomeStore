299f03e799867c4c50a0306387dcc8b2c1f8ba94
kovalev <kov92av@gmail.com>
2022-03-07 22:32:38 +0300

    added account controller and view

---
+++ HomeStore/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using HomeStore.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<IdentityUser> userManager;
+        private SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await userManager.FindByNameAsync(loginViewModel.Name);
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    if ((await signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false)).Succeeded)
+                    {
+                        return Redirect(loginViewModel?.ReturnUrl ?? "/Admin");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Неверный пароль или имя");
+            return View(loginViewModel);
+        }
+
+        [Authorize]
+        public async Task<RedirectResult> Loguot(string returnUrl = "/")
+        {
+            await signInManager.SignOutAsync();
+            return Redirect(returnUrl);
+        }
+    }
+}
